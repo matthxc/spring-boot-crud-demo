493042329e726d6cd976443b9beeb80ce510c4db
jcamargoramirez <jcamargoramirez@hubspot.com>
2021-06-11 17:45:30 -0500

    feat: add graphql support and query schema

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import graphql.kickstart.tools.GraphQLQueryResolver;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService {
+public class EmployeeServiceImpl implements EmployeeService, GraphQLQueryResolver {
     private EmployeeRepository employeeRepository;
 
     @Autowired
