dcfd4d2867ef90618f67ff460b43d2a30b564cfe
jcamargoramirez <jcamargoramirez@hubspot.com>
2021-07-16 17:27:48 -0500

    feat: enable eureka client

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -2,8 +2,10 @@ package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
